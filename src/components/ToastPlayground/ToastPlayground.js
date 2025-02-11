a983d212f2265d7d848c50c10e3eaec61abd3f71
EmanuelDuca <edu@usekeyhole.com>
2025-02-11 21:58:46 +0100

    Working verion of Toast component

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../Button';
+import Button from "../Button";
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css";
+//import Toast from "../Toast/Toast";
+import ToastShelf from "../ToastShelf/ToastShelf";
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
+  const [variant, setVariant] = React.useState();
+  const [message, setMessage] = React.useState();
+
+  const [toastArray, setToastArray] = React.useState([
+    {
+      variant: "notice",
+      text: "Example notice toast",
+    },
+    {
+      variant: "error",
+      text: "Example error toast",
+    },
+  ]);
+
+  console.log(toastArray);
+
+  function onToastSelect(toastId) {
+    console.log(toastId);
+    const newArray = toastArray.filter((_, index) => index !== toastId);
+    setToastArray(newArray);
+  }
+
+  function createNewToast() {
+    setToastArray([...toastArray, { variant: variant, text: message }]);
+    setMessage("");
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -14,45 +43,54 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      <ToastShelf toastArray={toastArray} onToastSelect={onToastSelect} />
+
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((item, index) => {
+              return (
+                <label htmlFor={`variant-${item}`} key={`variant-${item}`}>
+                  <input
+                    id={`variant-${item}`}
+                    type="radio"
+                    name="variant"
+                    value={item}
+                    onClick={() => {
+                      setVariant(item);
+                    }}
+                  />
+                  {item}
+                </label>
+              );
+            })}
             {/* TODO Other Variant radio buttons here */}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button>Pop Toast!</Button>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            <Button onClick={() => createNewToast()}>Pop Toast!</Button>
           </div>
         </div>
       </div>
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
 
-function ToastShelf() {
+function ToastShelf({ toastArray, onToastSelect }) {
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {toastArray.map((item, index) => {
+        return (
+          <li key={index} className={styles.toastWrapper}>
+            <Toast
+              variant={item.variant}
+              text={item.text}
+              id={index}
+              onClose={onToastSelect}
+            />
+          </li>
+        );
+      })}
     </ol>
   );
 }
